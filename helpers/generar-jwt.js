49e5528b6f58c1838b42ea2a88bc35c2e12968ab
JuanCarlosBajan <baj20109@uvg.edu.gt>
2022-03-30 19:23:25 -0600

    generate token function

---
+++ helpers/generar-jwt.js
@@ -0,0 +1,27 @@
+
+import jwt from 'jsonwebtoken'
+
+export const generarJWT = ( uid = '') => {
+
+    return new Promise( (resolve, reject) => {
+
+        const payload = { uid }
+
+        jwt.sign( payload, process.env.SECRETORPRIVATEKEY, {
+            expiresIn: '1d'
+        }, 
+            ( err, token ) => {
+
+                if (err) {
+                    console.log(err);
+                    reject('no se pudo generar el token');
+                } else {
+                    resolve( token );
+                }
+
+            }
+        )
+
+    } )
+
+}
