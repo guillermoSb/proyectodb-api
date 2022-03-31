b2907c10ef2e6898a287c193e42859e851c82e7c
JuanCarlosBajan <baj20109@uvg.edu.gt>
2022-03-31 12:38:48 -0600

    Create validar-jwt.js

---
+++ middlewares/validar-jwt.js
@@ -0,0 +1,43 @@
+import { getUser } from '../models/user.js';
+import { response, request } from 'express';
+import jwt from 'jsonwebtoken'
+
+export const validarJWT = async ( req = response, res = request, next) => {
+
+    const token = req.header('x-token');
+
+    if (!token) {
+        return res.status(401).json({
+            msg: 'No hay token en la petición'
+        });
+    }
+
+
+    try {
+
+        const { uid } = jwt.verify( token, process.env.SECRETORPRIVATEKEY );
+
+        req.usuario = await getUser( uid );
+
+        if (!req.usuario) {
+            return res.status(401).send({
+                ok: false,
+                errors: [
+                    'Token no válido'
+                ]
+            })
+        }
+
+        req.uid = uid;
+
+        next();
+        
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({
+            msg: 'Token no válido'
+        });
+        
+    }
+
+}
