e076278cff94bf7480b18c4d66ac98c72959fb85
JuanCarlosBajan <baj20109@uvg.edu.gt>
2022-04-01 01:08:49 -0600

    creacion modelo content

--- models/content.js
+++ models/content.js
@@ -1,12 +1,35 @@
 import { DatabaseManager } from '../database/manager.js';
 
 /**
- * @param {string} userCode
- * 
+ * @param {string} profileCode
  */
 
-export const getFavorites = (userCode) => {
+export const getAllFavoriteMovies = async (profileCode) => {
 
-    const movies = await DatabaseManager.knex('profileMovieFavorites').select('*').where({ userCode }).innerJoin('movies','profileMovieFavorites.movieCode','movies.movieCode');
+    const movies = await DatabaseManager.knex('favorites_movies').select('*').where({ profileCode }).innerJoin('movies','favorites_movies.movieCode','movies.movieCode');
+
+    return movies;
+}
+
+
+/**
+ * @param {string} genre
+ */
+
+export const getAllMoviesByGenre = async (genre) => {
+
+    const movies = await DatabaseManager.knex('movies').select('*').where({ genre });
+
+    return movies;
+
+}
+
+
+
+ export const getAllGenres = async () => {
+
+    const genres = await DatabaseManager.knex('genres').select('*');
+
+    return genres;
 
 }
