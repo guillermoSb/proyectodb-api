965036dccd1abe8a4f615318560651454c59a510
Guillermo Santos <guillermo.santosba@gmail.com>
2022-04-02 19:09:44 -0600

    Modified the add favorite route

--- routes/content.js
+++ routes/content.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { check,param } from 'express-validator';
+import { check, param } from 'express-validator';
 
 import { getFavouriteMovies, getMoviesByGenre, getMovies, addFavorite } from '../controllers/content.js';
 import { getAllGenres } from '../models/content.js';
@@ -7,27 +7,28 @@ import { validateUserExists } from '../utils/custom-validators.js';
 
 const router = Router();
 
-router.get('/',getMovies);
+router.get('/', getMovies);
 
 
-router.get('/:profileCode/favourites', 
+router.get('/:profileCode/favourites',
     [
         param('profileCode', 'El código de perfil debe ser un número').isNumeric(),
-        param('profileCode','El código de perfil es requerido').custom(validateUserExists)
+        param('profileCode', 'El código de perfil es requerido').custom(validateUserExists)
     ],
     getFavouriteMovies);
 
 
-router.get('/:genre/movies', 
+router.get('/:genre/movies',
     [
-        param('genre','El código de usuario es requerido').isIn(getAllGenres)
+        param('genre', 'El código de usuario es requerido').isIn(getAllGenres)
     ],
     getMoviesByGenre);
 
-router.post('/addFavorite',
+router.post('/:profileCode/favourites',
     [
-        check('profileCode','El código de perfil no es válido').isNumeric(),
-        check('movieCode','El código de película no es válido').isNumeric()
+        check('profileCode', 'El código de perfil no es válido').isNumeric(),
+        check('movieCode', 'El código de película no es válido').isNumeric()
     ],
     addFavorite);
+
 export default router;
