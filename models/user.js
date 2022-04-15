8b2f643fd0d604d48ef3d937fa6352e2643770a4
JuanCarlosBajan <baj20109@uvg.edu.gt>
2022-04-14 19:17:45 -0500

    Update user.js

--- models/user.js
+++ models/user.js
@@ -192,12 +192,10 @@ export const downgradeUser = async (userCode) => {
         await DatabaseManager.knex('users').update('plan',name).where({userCode});
         const profiles = await DatabaseManager.knex('profiles').where({userCode, active:true});
         const profileCountByPlan = await DatabaseManager.knex('plans').select('profileCount').where({name});
-        const profilesCodeToBeDeactivated = await DatabaseManager.knex('profiles').select('profileCode').where({userCode, active:true}).limit(Object.keys(profiles).length-(profileCountByPlan)[0].profileCount);
-
-        const asd = profilesCodeToBeDeactivated.map(value => {return Object.values(value)[0]})
-        console.log(asd);
+        const profilesCodeToBeDeactivatedFormat = await DatabaseManager.knex('profiles').select('profileCode').where({userCode, active:true}).limit(Object.keys(profiles).length-(profileCountByPlan)[0].profileCount);
+        const profilesCodeToBeDeactivated = profilesCodeToBeDeactivatedFormat.map(value => {return Object.values(value)[0]})
         if (Object.keys(profiles).length > (profileCount)[0].profileCount) {
-            await DatabaseManager.knex('profiles').update('active',false).where({userCode}).whereIn('profileCode',asd);
+            await DatabaseManager.knex('profiles').update('active',false).where({userCode}).whereIn('profileCode',profilesCodeToBeDeactivated);
         }
     }
 
