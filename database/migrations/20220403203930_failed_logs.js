95675218ca03c667186a161878f1a2a8adc312a0
JuanCarlosBajan <baj20109@uvg.edu.gt>
2022-04-16 17:14:55 -0500

    Create 20220403203930_failed_logs.js

---
+++ database/migrations/20220403203930_failed_logs.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema
+        .createTable('failed_logs', (table) => {
+            table.string('email').notNullable();
+            table.double('password').notNullable();
+        })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
